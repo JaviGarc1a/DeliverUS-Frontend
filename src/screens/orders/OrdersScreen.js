6e5976f04b53fe211c2360660d41650941eeeff6
Javier García Aguilar <javiergarciaaguilar2002@gmail.com>
2023-02-23 20:49:42 +0100

    OrderScreen finalizada

--- src/screens/orders/OrdersScreen.js
+++ src/screens/orders/OrdersScreen.js
@@ -1,33 +1,85 @@
-import React from 'react'
-import { StyleSheet, View, Pressable } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { StyleSheet, View, FlatList } from 'react-native'
+import { getOrder } from '../../api/OrderEndPoints'
 import TextRegular from '../../components/TextRegular'
 import TextSemiBold from '../../components/TextSemibold'
-import { brandPrimary, brandPrimaryTap, brandSecondary } from '../../styles/GlobalStyles'
+import { brandPrimary, brandSecondary, flashTextStyle, flashStyle } from '../../styles/GlobalStyles'
+import { showMessage } from 'react-native-flash-message'
+import ImageCard from '../../components/ImageCard'
+import { AuthorizationContext } from '../../context/AuthorizationContext'
 
 export default function OrdersScreen ({ navigation }) {
+  const [orders, setOrders] = useState([])
+  const { loggedInUser } = useContext(AuthorizationContext)
+
+  useEffect(() => {
+    async function fetchOrder () {
+      try {
+        const fetchedOrder = await getOrder()
+        setOrders(fetchedOrder)
+      } catch (error) {
+        showMessage({
+          message: `There was an error while retrieving orders. ${error} `,
+          type: 'error',
+          style: flashStyle,
+          titleStyle: flashTextStyle
+        })
+      }
+    }
+    if (loggedInUser) {
+      fetchOrder()
+    } else {
+      setOrders(null)
+    }
+  }, [loggedInUser])
+
+  const renderHeader = () => {
+    return (
+      <View style={styles.container}>
+              <TextSemiBold>FR5: Listing my confirmed orders</TextSemiBold>
+              <TextRegular>A Customer will be able to check his/her confirmed orders, sorted from the most recent to the oldest.</TextRegular>
+              <TextRegular textStyle={styles.text}>Go to Order Detail Screen</TextRegular>
+          </View>
+    )
+  }
+
+  const renderOrder = ({ item }) => {
+    const date = Date(item.createdAt).toString().replace('GMT+0200 (hora de verano de Europa central)', '')
+    return (
+      <ImageCard
+      imageUri={item.restaurant.logo ? { uri: process.env.API_BASE_URL + '/' + item.restaurant.logo } : undefined}
+      title={item.restaurant.name}
+      onPress={() => {
+        navigation.navigate('OrderDetailScreen', { id: item.id })
+      }}
+    >
+      <br/>
+      <TextSemiBold>Quantity: <TextSemiBold textStyle={{ color: brandPrimary }}>{item.products.length}</TextSemiBold></TextSemiBold>
+      <TextSemiBold>Price: <TextSemiBold textStyle={{ color: brandPrimary }}>{item.price}€</TextSemiBold></TextSemiBold>
+      <TextSemiBold>Shipping: <TextSemiBold textStyle={{ color: brandPrimary }}>{item.shippingCosts.toFixed(2)}€</TextSemiBold></TextSemiBold>
+      <TextSemiBold>Date: <TextSemiBold textStyle={{ color: brandPrimary }}>{date}</TextSemiBold></TextSemiBold>
+
+    </ImageCard>
+    )
+  }
+
+  const renderEmptyOrdersList = () => {
+    return (
+      <TextRegular textStyle={styles.emptyList}>
+        This user has no orders yet.
+      </TextRegular>
+    )
+  }
+
   return (
-        <View style={styles.container}>
-            <TextSemiBold>FR5: Listing my confirmed orders</TextSemiBold>
-            <TextRegular>A Customer will be able to check his/her confirmed orders, sorted from the most recent to the oldest.</TextRegular>
-            <Pressable
-                onPress={() => {
-                  navigation.navigate('OrderDetailScreen', { id: Math.floor(Math.random() * 100) })
-                }}
-                style={({ pressed }) => [
-                  {
-                    backgroundColor: pressed
-                      ? brandPrimaryTap
-                      : brandPrimary
-                  },
-                  styles.button
-                ]}
-            >
-                <TextRegular textStyle={styles.text}>Go to Order Detail Screen</TextRegular>
-            </Pressable>
-        </View>
+      <FlatList
+        ListHeaderComponent={renderHeader}
+        ListEmptyComponent={renderEmptyOrdersList}
+        data={orders}
+        renderItem={renderOrder}
+      />
   )
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
